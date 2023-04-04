33a0c03cc3a19519ea4a6c9be5b18ee074253e17
Kalyan1219 <kalyankal83@gmail.com>
2023-04-04 12:23:22 +0530

    Hello CallBack

---
+++ CallBack.js
@@ -0,0 +1,26 @@
+// const { get } = require("express/lib/response")
+
+const posts=[
+    { title:"First Post"},
+    { title:"Second Post"},
+    { title:"Third Post"}
+]
+
+function getPost(){
+    setTimeout(()=>{
+        let output=''
+        posts.forEach((values,index)=>{
+            output+=`<li>${values.title}</li>`
+        })
+        document.body.innerHTML=output
+
+    },1000)   
+}
+
+function createPost(post,callback){
+    setTimeout(()=>{
+        posts.push(post)
+        callback()
+    },2000)
+}
+createPost({title:"Forth Post"},getPost)
